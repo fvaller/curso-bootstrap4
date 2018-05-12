cfc898ad4db61b44d4448a9b2ca261c7adf54b52
Fernando Valler <fernandovaller@gmail.com>
2018-05-12 15:26:51 -0300

    add all files

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Curso de Bootstrap 4</title>
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		
+		<nav class="navbar navbar-expand-lg navbar-dark bg-gradient-info">
+
+			<div class="container">
+				<a href="index.php" class="navbar-brand h1 mb-0">Curso Bootstrap 4</a>
+
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+
+				<div class="collapse navbar-collapse" id="navbarSite">
+					<ul class="navbar-nav mr-auto">
+						<li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+						<li class="nav-item"><a href="#" class="nav-link">Perfil</a></li>
+						<li class="nav-item"><a href="#" class="nav-link">Serviços</a></li>
+						<li class="nav-item"><a href="#" class="nav-link">Depoimentos</a></li>
+						<li class="nav-item"><a href="#" class="nav-link">Contato</a></li>
+					</ul>
+					<ul class="navbar-nav ml-auto mr-4">
+						<li class="nav-item dropdown">
+							<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" id="navDrop">Social</a>
+							<div class="dropdown-menu">
+								<a class="dropdown-item" href="#">Facebook</a>
+								<a class="dropdown-item" href="#">Twitter</a>
+								<a class="dropdown-item" href="#">Instagram</a>
+								<a class="dropdown-item" href="#">Youtube</a>
+							</div>
+						</li>
+					</ul>
+					<form action="" class="form-inline">
+						<input type="search" name="" class="form-control mr-2" placeholder="Pesquisar">
+						<button class="btn btn-default" type="submit">Ok</button>
+					</form>
+				</div>
+
+			</div>
+		</nav>	
+
+		<div class="carousel slide" data-ride="carousel" id="carouselSite">
+
+			<ol class="carousel-indicators">
+				<li data-target="#carouselSite" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselSite" data-slide-to="1"></li>			
+			</ol>
+
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img src="images/slider2.jpg" class="img-fluid d-block" >
+					<div class="carousel-caption d-none d-md-block">
+						<h3>Lorem ipsum dolor sit amet</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum possimus autem praesentium sapiente asperiores.</p>
+					</div>
+				</div>
+
+				<div class="carousel-item">
+					<img src="images/slider3.jpg" class="img-fluid d-block">
+					<div class="carousel-caption d-none d-md-block">
+						<h3>Consectetur Adipisicing Elit</h3>
+						<p>Lorem ipsum dolor sit amet, Nostrum possimus autem praesentium sapiente asperiores.</p>
+					</div>					
+				</div>
+			</div>
+			<a href="#carouselSite" class="carousel-control-prev" data-slide="prev">
+				<span class="carousel-control-prev-icon"></span>
+				<span class="sr-only">Anterior</span>
+			</a>
+
+			<a href="#carouselSite" class="carousel-control-next" data-slide="next">
+				<span class="carousel-control-next-icon"></span>
+				<span class="sr-only">Avançar</span>
+			</a>
+		</div>
+
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="node_modules/jquery/dist/jquery.js"></script>
+		<script src="node_modules/popper.js/dist/umd/popper.js"></script>
+		<script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+	</body>
+</html>
